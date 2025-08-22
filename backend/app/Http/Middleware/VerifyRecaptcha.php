81ecee78b4ebf3634032a930af4264544b98bad4
Almahsol Tabigue <you@toyoflex.com>
2025-08-22 11:08:24 +0800

    fix the error

--- backend/app/Http/Middleware/VerifyRecaptcha.php
+++ backend/app/Http/Middleware/VerifyRecaptcha.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 
-class VerifyRecaptchaOptimized
+class VerifyRecaptcha
 {
     public function handle(Request $request, Closure $next)
     {
